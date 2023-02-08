f54d89c790690b8aaee61ade90b43a121daf58ab
xup6u04cj86 <yenhualin@tamu.edu>
2023-02-08 01:19:56 -0600

    v3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -24,20 +24,45 @@ end
 
 def sum_to_n?(arr, number)
   # YOUR CODE HERE
+  if arr.empty?
+    return false
+  elsif arr.length == 1
+    return false
+  else
+    for i in arr
+      if arr.include?(number-i)
+        if number-i != i
+          return true
+        end
+      end
+    end
+    return false
+  end  
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant?(string)
   # YOUR CODE HERE
+  return string[0] =~ /[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/
 end
 
 def binary_multiple_of_4?(string)
   # YOUR CODE HERE
+  if string =~ /[^0-1]/
+    return false
+  elsif string.empty?
+    return false
+  elsif string.to_i(2) % 4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
